c37ee8bec01328916a148a9b9a810956e6fb0c43
hunghx <120539776+hunghx@users.noreply.github.com>
2023-07-24 21:32:28 +0700

    Add files via upload

--- thietkecsdl.sql
+++ thietkecsdl.sql
@@ -27,7 +27,13 @@ foreign key (malh) references classes(malh)
 );
 -- them mới dữu liệu vào 
 insert into Student(masv,tensv,tuoi,diachi,malh) value
-(4,"Nguyễn Văn C",16,"Hà Nội",3);
+(13,"Nguyễn Văn M",16,"Hà Nội",null),
+(5,"Nguyễn Văn A",16,"Hà Nội",3),
+(6,"Nguyễn Văn B",16,"Hà Nội",3),
+(7,"Nguyễn Văn E",16,"Hà Nội",3),
+(8,"Nguyễn Văn F",16,"Hà Nội",3),
+(9,"Nguyễn Văn G",16,"Hà Nội",3),
+(10,"Nguyễn Văn H",16,"Hà Nội",3);
 
 -- 1 , "Nguyễn Văn B", 22, "Nghệ An",2, "Lớp 11A1"
 
@@ -41,3 +47,33 @@ createdDate datetime
 );
 
 insert into product(name, price, createdDate) values ("Quần âu",200,now());
+
+
+
+-- Truy vấn đầy đủ 
+-- SELECT , FROM , WHERE, GROUP BY, HAVING, ORDER BY 
+-- SELECT  :  Lấy những cột nào của bảng
+--  FROM  : từ những bảng nào 
+--  WHERE : điều kiên lấy là gì
+--  GROUP BY : dữ liệu có cần nhóm lại để tính toán và xử lí không 
+--  HAVING : điều kiện lấy sau khi đã nhóm dữ liệu (đi kèm với group by)
+--  ORDER BY  : dữ liệu được sắp xếp theo chiều nào
+
+select s.tuoi,s.masv,s.tensv from student as s where s.tensv like "%__d%"; 
+-- % đại điện cho 1 chuỗi bất kì
+-- _ đại diện cho 1 kí tự 
+ 
+ select s.tuoi,s.masv,s.tensv from student as s where s.masv in (1,3,5,7,9); 
+-- nhiều điều kiện OR / AND (||  &&)
+select s.tuoi,s.masv,s.tensv from student as s where (s.masv < 6) or (s.masv >8); 
+
+-- Mệnh đề JOIN để kết hợp dữ liệu của nhiều bảng 
+select * from Classes c join student s  on c.malh = s.malh ; 
+-- INNER JOIN : chỉ lấy dữ liệu chung của 2 bảng
+-- LEFT JOIN : Lấy toàn bộ bản ghi bên bảng tay trái mà không cần điểm chung của bảng bên tay phải
+-- RIGHT  JOIN : Lấy toàn bộ bản ghi bên bảng tay phải mà không cần điểm chung của bảng bên tay trái
+-- FULL OUTER  JOIN : Lấy toàn bộ bản ghi bên bảng tay phải và trái mà không cần khớp dữ liệu
+
+select * from Classes c right join student s  on c.malh = s.malh
+union
+select * from Classes c left join student s  on c.malh = s.malh;
